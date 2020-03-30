dd137a6e33aea7ed3d650bda8ccfe169b07495d8
fangjy <17112598@suning.com>
2020-03-30 15:24:01 +0800

    step by step 2

--- src/core/observer/array.js
+++ src/core/observer/array.js
@@ -28,6 +28,17 @@ const methodsToPatch = [
 methodsToPatch.forEach(function (method) {
   // cache original method
   const original = arrayProto[method]
+  // def
+
+  // export function def (obj: Object, key: string, val: any, enumerable?: boolean) {
+  //   Object.defineProperty(obj, key, {
+  //     value: val,
+  //     enumerable: !!enumerable,
+  //     writable: true,
+  //     configurable: true
+  //   })
+  // }
+  
   def(arrayMethods, method, function mutator (...args) {
     const result = original.apply(this, args)
     const ob = this.__ob__
